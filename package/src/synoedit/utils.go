f6462aafa0d5d87ca2beb2dddb2627653b51a0be
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-08-14 09:12:54 +1000

    Add descriptions and fixes

--- package/src/synoedit/utils.go
+++ package/src/synoedit/utils.go
@@ -18,9 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import (
-	"os"
 	"os/exec"
-	"path/filepath"
 )
 
 // CheckCmdExists looks for command in $PATH
