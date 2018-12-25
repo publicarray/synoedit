004550c8f6e5cb53463ba14bf6c536cf9c6d4d0a
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-12-25 13:57:28 +1100

    fix godoc

--- package/src/synoedit/main.go
+++ package/src/synoedit/main.go
@@ -15,6 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+// Synology package for editing files through a web interface
 package main
 
 import (
