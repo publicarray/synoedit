c5c163575480870b58115c659f7a0df81bb22071
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-07-08 21:25:47 +1000

    Update auth for DSM7

--- package/src/synoedit/auth.go
+++ package/src/synoedit/auth.go
@@ -19,12 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"os"
 	"os/exec"
-	"regexp"
 )
 
 // AppPrivilege is part of AuthJSON
@@ -43,29 +38,6 @@ type AuthJSON struct {
 	AppPrivilege AppPrivilege
 }
 
-// Retrieve login status and try to retrieve a CSRF token.
-// If either fails than we return an error to the user that they need to login.
-// Returns username or error
-func token() (string, error) {
-	cmd := exec.Command("/usr/syno/synoman/webman/login.cgi")
-	cmdOut, err := cmd.Output()
-	if err != nil && err.Error() != "exit status 255" { // in the Synology world, error code 255 apparently means success!
-		return string(cmdOut), err
-	}
-	// cmdOut = bytes.TrimLeftFunc(cmdOut, findJSON)
-
-	// Content-Type: text/html [..] { "SynoToken" : "GqHdJil0ZmlhE", "result" : "success", "success" : true }
-	r, err := regexp.Compile("SynoToken\" *: *\"([^\"]+)\"")
-	if err != nil {
-		return string(cmdOut), err
-	}
-	token := r.FindSubmatch(cmdOut)
-	if len(token) < 1 {
-		return string(cmdOut), errors.New("sorry, you need to login first")
-	}
-	return string(token[1]), nil
-}
-
 // Detect if the rune (character) contains '{' and therefore is likely to contain JSON
 // returns bool
 func findJSON(r rune) bool {
@@ -77,43 +49,19 @@ func findJSON(r rune) bool {
 
 // Check if the logged in user is Authorised or Admin.
 // If either fails than we return a HTTP Unauthorized error.
-func auth() {
-	token, err := token()
-	if err != nil {
-		logUnauthorised(err.Error())
-	}
-
-	// X-SYNO-TOKEN:9WuK4Cf50Vw7Q
-	// http://192.168.1.1:5000/webman/3rdparty/DownloadStation/webUI/downloadman.cgi?SynoToken=9WuK4Cf50Vw7Q
-	tempQueryEnv := os.Getenv("QUERY_STRING")
-	os.Setenv("QUERY_STRING", "SynoToken="+token)
-	cmd := exec.Command("/usr/syno/synoman/webman/modules/authenticate.cgi")
-	user, err := cmd.Output()
+func auth() string {
+	user, err := exec.Command("/usr/syno/synoman/webman/modules/authenticate.cgi").Output()
 	if err != nil && string(user) == "" {
-		logUnauthorised(err.Error())
-	}
-
-	// check permissions
-	if FileExists("/usr/syno/synoman/webman/initdata.cgi") {
-		cmd = exec.Command("/usr/syno/synoman/webman/initdata.cgi") // performance hit
-		cmdOut, err := cmd.Output()
-		if err != nil {
-			logUnauthorised(err.Error())
-		}
-		cmdOut = bytes.TrimLeftFunc(cmdOut, findJSON)
-
-		var jsonData AuthJSON
-		if err := json.Unmarshal(cmdOut, &jsonData); err != nil { // performance hit
-			logUnauthorised(err.Error())
-		}
-
-		isAdmin := jsonData.Session.IsAdmin              // Session.IsAdmin:true
-		isPermitted := jsonData.AppPrivilege.IsPermitted // AppPrivilege.SYNO.SDS.DNSCryptProxy.Application:true
-		if !(isAdmin || isPermitted) {
-			notFound()
+		if err.Error() == "exit status 7" {
+			// logUnauthorised("exec()", "You are probably logged in to the DSM but I don't have a cookie :'(")
+			// login to the web api:
+			//https://your-ip:5001/webapi/auth.cgi?api=SYNO.API.Auth&version=3&method=login&account=admin&passwd=your_admin_password&format=cookie
+			return ""
+		} else if err.Error() == "exit status 5" {
+			logUnauthorised("exec()", "You are unauthorised")
+		} else {
+			logUnauthorised("exec()", err.Error())
 		}
 	}
-
-	os.Setenv("QUERY_STRING", tempQueryEnv)
-	return
+	return string(user)
 }
